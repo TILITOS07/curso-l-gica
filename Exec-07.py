4d9ba3091477e2cc1dfc08866b7821bfdd378c00
Davi Silva <apululud@gmail.com>
2024-08-29 21:27:55 -0300

    Create Exec-07.py

---
+++ Exec-07.py
@@ -0,0 +1,12 @@
+#exercicio7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+print("       X")
+print("      XXX")
+print("     XXXXX")
+print("    XXXXXXX")
+print("   XXXXXXXXX")
+print("  XXXXXXXXXXX")
+print(" XXXXXXXXXXXXX")
+print("XXXXXXXXXXXXXXX")
+print("       XX")
+print("       XX")
+print("      XXXX")
