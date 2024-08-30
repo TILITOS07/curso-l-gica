4612b38822aa2cce8e519b1d1057db369aedfc08
Davi Silva <apululud@gmail.com>
2024-08-29 21:24:50 -0300

    Create Exec-03.py

---
+++ Exec-03.py
@@ -0,0 +1,2 @@
+#exercicio3 Letra de música - Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+print("Eu vivo pela fé, não por mim mesmo nem do que vejo")
