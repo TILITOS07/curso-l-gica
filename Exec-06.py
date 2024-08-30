d85dec3fdc7a70b1a0510098ccf3d5e7f0457cba
Davi Silva <apululud@gmail.com>
2024-08-29 21:26:59 -0300

    Create Exec-06.py

---
+++ Exec-06.py
@@ -0,0 +1,7 @@
+#exercicio6 Menu - Elabore um programa que mostre o seguinte menu na tela:
+print("cadastro do cliente")
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
