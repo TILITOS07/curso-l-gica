88f31fc30d83e80d4a727cbc1a59bae2242a59da
Davi Silva <apululud@gmail.com>
2024-08-29 21:24:10 -0300

    Create Exec-02.py

---
+++ Exec-02.py
@@ -0,0 +1,5 @@
+#exercicio2Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Davi silva")
+print("Rio de janeiro")
+print("Duque de caxias")
